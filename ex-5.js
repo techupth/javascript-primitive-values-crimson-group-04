a62339d5c6b6a0881d3d95a858207b52188cbe2d
Nitisit Vithunat <nitisitvithunat@MacBook-Pro-khxng-Nitisit.local>
2023-11-23 15:35:49 +0700

    feat:add assignment variable value

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let disount = (totalPrice * 25) / 100;
+let priceAfterdiscount = totalPrice - disount;
+console.log(priceAfterdiscount);
